9a0c398a5e214f5b84745ad866b16c70447a1a3b
Matthew Astley <yulu$mca@t8o.org>
2012-07-09 08:20:38 +0100

    "installer" update
    show not-installed files;
    include git-rhH under the scary option
      I want to install it without futzing
      you may not want to until you've seen what it does with no args

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+#! /bin/bash
+# Process substitution
 
 do_inst() {
-    local src dest leaf
+    local src dest instand leaf
     dest="$1"
+    instand="$2"
     src="$PWD"
     if ! [ -d "$dest" ] || ! [ -w "$dest" ]; then
 	echo "$0: $dest is not a writable directory" >&2
@@ -10,19 +12,35 @@ do_inst() {
     fi
 
     ln -snv "$src/bin" "$dest/,git-yacontrib"
-    for leaf in $( cat "$src/install.txt" ); do
+    for leaf in $( cat "$src/$instand" ); do
 	ln -snv ",git-yacontrib/$leaf" "$dest"
     done
 }
 
+show_skipped() {
+    local src
+    src="$PWD"
+    diff -u \
+	<( LC_ALL=C sort "$src"/install*.txt ) \
+	<( find bin -type f -printf "%f\n" | LC_ALL=C sort )
+}
+
 case "$1" in
-    -y) do_inst "$2" ;;
+    -y) do_inst "$2" install.txt ;;
+    -yS) do_inst "$2" install-scary.txt ;;
 #    -h | --help)
+    -s|--skipped) show_skipped ;;
     *)
-	printf "usage: ./%s -y ~/bin/\n
-Writes symlinks into the specified directory.\n
+	printf "usage: ./%s < -y ~/bin/ | --skipped >\n
+ -s | --skipped
+        List files not installed
+
+ -y     Write symlinks (ln -snv) into the specified directory.\n
+ -yS    Install (only) the scary \"read it before running\" shortcuts.\n
+
 This isn't so much an install, as placing the files the author likes
-on \$PATH the way he does it.\n" \
+on \$PATH the way he does it.\n
+It is ideminpotent: it will not overwrite existing files or links.\n" \
     "$( basename $0 )"
 	;;
 esac
