0078cb2f63b64f5de9b3217e9d3a5ec978f3ce94
Matthew Astley <codeonly$mca@t8o.org>
2017-03-01 09:17:01 +0000

    installer improvement

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #! /bin/bash
 # Process substitution
 
+show_syntax() {
+    printf "usage: ./%s < -y ~/bin/ | --skipped >\n
+ -s | --skipped
+        List files not installed
+
+ -y     Write symlinks (ln -snv) into the specified directory.\n
+ -yS    Install (only) the scary \"read it before running\" shortcuts.\n
+
+This isn't so much an install, as placing the files the author likes
+on \$PATH the way he does it.\n
+It is ideminpotent: it will not overwrite existing files or links.\n
+After the first install, the install destination is remembered and
+can be omitted to install additional files.\n" \
+    "$( basename $0 )"
+}
+
 do_inst() {
     local src dest instand leaf
     dest="$1"
     instand="$2"
+    if [ -z "$dest" ]; then
+	dest="$( git config plumbed-in-from.PATH )"
+	[ -z "$dest" ] && show_syntax
+	dest="$( dirname "$dest" )"
+	echo "Using previous install directory $dest"
+    fi
     src="$PWD"
     if ! [ -d "$dest" ] || ! [ -w "$dest" ]; then
 	echo "$0: $dest is not a writable directory" >&2
@@ -33,17 +55,5 @@ case "$1" in
     -yS) do_inst "$2" install-scary.txt ;;
 #    -h | --help)
     -s|--skipped) show_skipped ;;
-    *)
-	printf "usage: ./%s < -y ~/bin/ | --skipped >\n
- -s | --skipped
-        List files not installed
-
- -y     Write symlinks (ln -snv) into the specified directory.\n
- -yS    Install (only) the scary \"read it before running\" shortcuts.\n
-
-This isn't so much an install, as placing the files the author likes
-on \$PATH the way he does it.\n
-It is ideminpotent: it will not overwrite existing files or links.\n" \
-    "$( basename $0 )"
-	;;
+    *) show_syntax ;;
 esac
